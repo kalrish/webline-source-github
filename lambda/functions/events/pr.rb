b27f309bd5b745beb4c905463d66b9d6bb69c279
David Joaquín Shourabi Porcel <david@djsp.eu>
2019-09-15 15:30:47 +0200

    FIXME

--- lambda/functions/events/pr.rb
+++ lambda/functions/events/pr.rb
@@ -1,10 +1,8 @@
 # Handles PR-related events
 
 
-require 'aws-sdk-cloudformation'
 require 'aws-sdk-s3'
 require 'json'
-require 'octokit'
 
 
 def upload_configuration(bucket:, owner:, repository:, branch:)
@@ -28,54 +26,15 @@ def upload_configuration(bucket:, owner:, repository:, branch:)
 end
 
 
-def pipeline_deployed?(branch:, repository:)
-  client = Aws::CloudFormation::Client.new()
+def notify_github(result:)
+  # Changeset empty
+  message = 'Pipeline triggered.'
 
-  response = {}
+  # Changeset not empty
+  message = ''
 
-  pipeline_stack_exists = false
-
-  return pipeline_stack_exists
-end
-
-
-def deploy_pipeline(bucket:, role:, sns_arn:)
-  client = Aws::CloudFormation::Client.new()
-
-  response = client.create_stack(
-    stack_name: "webpipe-pipeline-#{owner}-#{repository}-#{branch}",
-    template_url: "https://#{bucket}.s3.amazonaws.com/v1/cfn/pipeline.yaml",
-    parameters: [
-      {
-        parameter_key: 'WebpipeBucket',
-        parameter_value: bucket,
-      },
-      {
-        parameter_key: 'Branch',
-        parameter_value: branch,
-      },
-      {
-        parameter_key: 'SourceProvider',
-        parameter_value: 'github',
-      },
-    ],
-    capabilities: [
-      'CAPABILITY_IAM',
-    ],
-    role_arn: role,
-    notification_arns: [
-      sns_arn,
-    ],
-    tags: [
-      {
-        key: 'GitHub-Owner',
-        value: owner,
-      },
-      {
-        key: 'GitHub-Repo',
-        value: repo,
-      },
-    ],
+  call_integration_github_commenter(
+    message: message,
   )
 end
 
@@ -89,39 +48,16 @@ def process_event(event_data)
   repository = event_data['repository']['name']
   pr_number = event_data['pull_request']['id']
 
-  role = get_customer_role_from_repo_branch()
-
-  assume_role(role)
-
   upload_configuration(
+    branch: branch,
     bucket: bridge_bucket,
     owner: owner,
     repository: repository,
-    branch: branch,
   )
 
-  pipeline_deployed = pipeline_deployed?(
-    branch: branch,
-    repository: repository,
-  )
-
-  if not pipeline_deployed?(branch)
-    deploy_pipeline(
-      bucket: webpipe_bucket,
-      role: pipeline_stack_deployment_role,
-      sns_arn: pipeline_stack_notification_topic,
-      owner: owner,
-      repository: repository,
-      branch: branch,
-    )
-  end
-
-  trigger_pipeline(
-  )
-
-  github_token = get_github_token(github_token_secret)
+  result = call_core_pipeline_deployer()
 
   notify_github_inprogress(
-    token: github_token,
+    result: result,
   )
 end
